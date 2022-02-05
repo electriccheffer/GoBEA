2fdbd1c84422a840bdb23cae4c37b6fe765e2331
chef <rmccaffe@uno.edu>
2022-02-05 07:30:41 -0600

    Fully implemented NIPADataRequest object with documentation.

--- src/Objects/Data/NIPA/NIPADataRequest.go
+++ src/Objects/Data/NIPA/NIPADataRequest.go
@@ -7,3 +7,28 @@ type NIPADataRequest struct {
 	frequency []string
 	years     []string
 }
+
+//TableId returns the value of the tableId field of the NIPADataRequest.
+func (r NIPADataRequest) TableId() int {
+	return r.tableId
+}
+
+//setTableId sets the value of the tableId field for a NIPADataRequest object.
+func (r NIPADataRequest) setTableId(newTableId int) {
+	r.tableId = newTableId
+}
+
+//Frequency returns an array of strings for the time series data.
+func (r NIPADataRequest) Frequency() []string {
+	return r.frequency
+}
+
+//setFrequency sets the value of the frequency field fo a NIPADataRequest object.
+func (r NIPADataRequest) setFrequency(newFrequencies []string) {
+	r.frequency = newFrequencies
+}
+
+//Years returns the array of years in the NIPADataRequest.
+func (r NIPADataRequest) Years() []string {
+	return r.years
+}

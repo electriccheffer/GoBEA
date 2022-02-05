7a7630062b33961396e0ba0196841dc580b55962
chef <rmccaffe@uno.edu>
2022-02-05 07:39:06 -0600

    NIPADataRequest allowing for the setting of all fields.  Empty test methods for.

---
+++ src/Objects/Data/NIPA/NIPADataRequest_test.go
@@ -0,0 +1,15 @@
+package NIPA
+
+import "testing"
+
+func TestNIPADataRequest_Frequency(t *testing.T) {
+
+}
+
+func TestNIPADataRequest_TableId(t *testing.T) {
+
+}
+
+func TestNIPADataRequest_Years(t *testing.T) {
+
+}

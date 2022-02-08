3a202660853ef972006620dd176642509721ac7a
rmccaf <rmichmccafffcsci2467@gmail.com>
2022-02-08 05:50:22 -0600

    IntlServTradeRequest with getters and setters implemented, untested, and undocumented

--- src/Objects/Data/IntlServTradeRequest/IntlServTradeRequest.go
+++ src/Objects/Data/IntlServTradeRequest/IntlServTradeRequest.go
@@ -6,3 +6,35 @@ type IntlServTradeRequest struct {
 	affiliation    string
 	areaOrCountry  string
 }
+
+func (i *IntlServTradeRequest) TypeOfService() string {
+	return i.typeOfService
+}
+
+func (i *IntlServTradeRequest) SetTypeOfService(typeOfService string) {
+	i.typeOfService = typeOfService
+}
+
+func (i *IntlServTradeRequest) TradeDirection() string {
+	return i.tradeDirection
+}
+
+func (i *IntlServTradeRequest) SetTradeDirection(tradeDirection string) {
+	i.tradeDirection = tradeDirection
+}
+
+func (i *IntlServTradeRequest) Affiliation() string {
+	return i.affiliation
+}
+
+func (i *IntlServTradeRequest) SetAffiliation(affiliation string) {
+	i.affiliation = affiliation
+}
+
+func (i *IntlServTradeRequest) AreaOrCountry() string {
+	return i.areaOrCountry
+}
+
+func (i *IntlServTradeRequest) SetAreaOrCountry(areaOrCountry string) {
+	i.areaOrCountry = areaOrCountry
+}

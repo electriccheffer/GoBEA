334c13e14f8622186ad3f10aa24e56df045935bb
rmccaf <rmichmccafffcsci2467@gmail.com>
2022-03-27 09:07:02 -0500

    Regional object with getters, setters, constructor, and toString method Tested: getters, setters

--- src/Objects/Data/Regional/Regional_test.go
+++ src/Objects/Data/Regional/Regional_test.go
@@ -84,4 +84,57 @@ func TestRegional_SetTableName(t *testing.T) {
 		t.Error("nullCase SetTableName")
 	}
 
+	nullCase.SetTableName("otherName")
+	if nullCase.TableName() != "otherName" {
+		t.Error("nullCase2 SetTableName")
+	}
+}
+
+func TestRegional_SetLineCode(t *testing.T) {
+	nullCase := Regional{
+		tableName: "",
+		lineCode:  0,
+		geoFips:   "",
+		year:      "",
+	}
+
+	nullCase.SetLineCode(20)
+	if nullCase.LineCode() != 20 {
+		t.Error("nullCase LineCode")
+	}
+	nullCase.SetLineCode(30)
+	if nullCase.LineCode() != 30 {
+		t.Error("nullCase2 LineCode")
+	}
+}
+
+func TestRegional_SetGeoFips(t *testing.T) {
+	nullCase := Regional{
+		tableName: "",
+		lineCode:  0,
+		geoFips:   "",
+		year:      "",
+	}
+	nullCase.SetGeoFips("newFip")
+	if nullCase.GeoFips() != "newFip" {
+		t.Error("nullCase GeoFips")
+	}
+
+}
+
+func TestRegional_SetYear(t *testing.T) {
+	nullCase := Regional{
+		tableName: "",
+		lineCode:  0,
+		geoFips:   "",
+		year:      "",
+	}
+	nullCase.SetYear("2003")
+	if nullCase.Year() != "2003" {
+		t.Error("nullCase SetYear")
+	}
+	nullCase.SetYear("2004")
+	if nullCase.Year() != "2004" {
+		t.Error("nullCase2 SetYear")
+	}
 }

8ddef34680d9079935ef170f3e2ad6c31673bbb9
rmccaf <rmichmccafffcsci2467@gmail.com>
2022-03-27 08:46:11 -0500

    Regional object with getters, setters, constructor, and toString method Tested: TableName getter

--- src/Objects/Data/Regional/Regional_test.go
+++ src/Objects/Data/Regional/Regional_test.go
@@ -1,1 +1,26 @@
 package Regional
+
+import "testing"
+
+func TestRegional_TableName(t *testing.T) {
+	nullCase := Regional{
+		tableName: "",
+		lineCode:  0,
+		geoFips:   "",
+		year:      "",
+	}
+	if nullCase.tableName != "" {
+		t.Error("null case TableName")
+	}
+
+	nonNullCase := Regional{
+		tableName: "Table",
+		lineCode:  2,
+		geoFips:   "392",
+		year:      "2005",
+	}
+	if nonNullCase.tableName != "Table" {
+		t.Error("nonNullCase TableName")
+	}
+
+}

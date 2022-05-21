f66b81dec87e7734a2016444db1b482e76e73760
rmccaf <rmichmccafffcsci2467@gmail.com>
2022-05-21 07:21:31 -0500

    IIPRequest.go with no documentation and no tests

---
+++ src/Objects/Data/InternationalInvestmentPosition/IIPRequest.go
@@ -0,0 +1,44 @@
+package InternationalInvestmentPosition
+
+type IIPRequest struct {
+	typeOfInvestment string
+	component        string
+	frequency        string
+	year             string
+}
+
+func (I *IIPRequest) TypeOfInvestment() string {
+	return I.typeOfInvestment
+}
+
+func (I *IIPRequest) SetTypeOfInvestment(typeOfInvestment string) {
+	I.typeOfInvestment = typeOfInvestment
+}
+
+func (I *IIPRequest) Component() string {
+	return I.component
+}
+
+func (I *IIPRequest) SetComponent(component string) {
+	I.component = component
+}
+
+func (I *IIPRequest) Frequency() string {
+	return I.frequency
+}
+
+func (I *IIPRequest) SetFrequency(frequency string) {
+	I.frequency = frequency
+}
+
+func (I *IIPRequest) Year() string {
+	return I.year
+}
+
+func (I *IIPRequest) SetYear(year string) {
+	I.year = year
+}
+
+func (I *IIPRequest) String() string {
+	return I.component + " " + I.typeOfInvestment + " " + I.frequency + " " + I.year
+}

99c7ab2fdf8fefad80242867017bbfada3724691
rmccaf <rmichmccafffcsci2467@gmail.com>
2022-05-25 05:38:17 -0500

    NIUnderlyingDetail struct with getters and setters, commented, untested.

--- src/Objects/Data/NIUnderlyingDetail/NIUnderlyingDetail.go
+++ src/Objects/Data/NIUnderlyingDetail/NIUnderlyingDetail.go
@@ -5,3 +5,43 @@ type NIUnderlyingDetailRequest struct {
 	frequency string
 	year      string
 }
+
+//TableName is a getter for an NIUnderlyingDetailRequest
+func (N *NIUnderlyingDetailRequest) TableName() string {
+	return N.tableName
+}
+
+//SetTableName is a setter for an NIUnderlyingDetailRequest
+func (N *NIUnderlyingDetailRequest) SetTableName(tableName string) {
+	N.tableName = tableName
+}
+
+//Frequency is a getter for an NIUnderlyingDetailRequest
+func (N *NIUnderlyingDetailRequest) Frequency() string {
+	return N.frequency
+}
+
+//SetFrequency is a setter for an NIUnderlyingDetailRequest
+func (N *NIUnderlyingDetailRequest) SetFrequency(frequency string) {
+	N.frequency = frequency
+}
+
+//Year is a getter for an NIUnderlyingDetailRequest
+func (N *NIUnderlyingDetailRequest) Year() string {
+	return N.year
+}
+
+//SetYear is a setter for an NIUnderlyingDetailRequest
+func (N *NIUnderlyingDetailRequest) SetYear(year string) {
+	N.year = year
+}
+
+//NewNIUnderlyingDetailRequest is a constuctor for anNIUnderlyingDetailRequest
+func NewNIUnderlyingDetailRequest(tableName string, frequency string, year string) *NIUnderlyingDetailRequest {
+	return &NIUnderlyingDetailRequest{tableName: tableName, frequency: frequency, year: year}
+}
+
+//String is a method to return a string version of an NewNIUnderlyingDetailRequest
+func (N NIUnderlyingDetailRequest) String() string {
+	return N.tableName + " " + N.frequency + " " + N.year
+}

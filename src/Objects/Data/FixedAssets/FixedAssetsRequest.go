e4ca8fab36dcb765e55c2ca0662909dd2816e6a6
rmccaf <rmichmccafffcsci2467@gmail.com>
2022-05-26 08:18:20 -0500

    Fixed Assets Request with getters and setters. No tests.  No documentation.

---
+++ src/Objects/Data/FixedAssets/FixedAssetsRequest.go
@@ -0,0 +1,35 @@
+package FixedAssets
+
+type FixedAssetsRequest struct {
+	tableName   string
+	year        string
+	requestType string
+}
+
+func (f *FixedAssetsRequest) TableName() string {
+	return f.tableName
+}
+
+func (f *FixedAssetsRequest) SetTableName(tableName string) {
+	f.tableName = tableName
+}
+
+func (f *FixedAssetsRequest) Year() string {
+	return f.year
+}
+
+func (f *FixedAssetsRequest) SetYear(year string) {
+	f.year = year
+}
+
+func (f *FixedAssetsRequest) RequestType() string {
+	return f.requestType
+}
+
+func (f *FixedAssetsRequest) SetRequestType(requestType string) {
+	f.requestType = requestType
+}
+
+func NewFixedAssetsRequest(tableName string, year string, requestType string) *FixedAssetsRequest {
+	return &FixedAssetsRequest{tableName: tableName, year: year, requestType: requestType}
+}

ca73220cb4d0505f1a37a66ed950a1b727ce9eda
rmccaf <rmichmccafffcsci2467@gmail.com>
2022-05-26 20:41:46 -0500

    Fixed Assets Request with getters, setters, tests, and documentation.

--- src/Objects/Data/FixedAssets/FixedAssetsRequest.go
+++ src/Objects/Data/FixedAssets/FixedAssetsRequest.go
@@ -6,34 +6,42 @@ type FixedAssetsRequest struct {
 	requestType string
 }
 
+//TableName is a getter for the tableName FixedAssetsRequest
 func (f *FixedAssetsRequest) TableName() string {
 	return f.tableName
 }
 
+//SetTableName is a setter for the tableName FixedAssetsRequest
 func (f *FixedAssetsRequest) SetTableName(tableName string) {
 	f.tableName = tableName
 }
 
+//Year is a getter for the year FixedAssetsRequest
 func (f *FixedAssetsRequest) Year() string {
 	return f.year
 }
 
+//SetYear is a setter for the year of a FixedAssetsRequest
 func (f *FixedAssetsRequest) SetYear(year string) {
 	f.year = year
 }
 
+//RequestType is a getter for the requestType of a FixedAssetsRequest
 func (f *FixedAssetsRequest) RequestType() string {
 	return f.requestType
 }
 
+//SetRequestType is a setter for the requestType of a FixedAssetsRequest
 func (f *FixedAssetsRequest) SetRequestType(requestType string) {
 	f.requestType = requestType
 }
 
+//NewFixedAssetsRequest is a constructor for a FixedAssetsRequest
 func NewFixedAssetsRequest(tableName string, year string, requestType string) *FixedAssetsRequest {
 	return &FixedAssetsRequest{tableName: tableName, year: year, requestType: requestType}
 }
 
+//String is a method which returns a string of the FixedAssetsRequest
 func (f *FixedAssetsRequest) String() string {
 	return f.tableName + " " + f.year + " " + f.requestType
 }
